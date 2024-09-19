d7c09fcf83c61df613f21b0e127781b96d34b546
Michael Galo <michael@michaelthinks.co>
2024-09-19 13:17:05 -0500

    reviews.py view created to support CRUD API functionality

--- raterapi/views/reviews.py
+++ raterapi/views/reviews.py
@@ -1,9 +1,18 @@
 from rest_framework import viewsets
 from rest_framework import serializers
 from raterapi.models import Review
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "first_name", "last_name")
 
 
 class ReviewsSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = Review
         fields = (
@@ -23,3 +32,6 @@ class ReviewsViewSet(viewsets.ModelViewSet):
 
     queryset = Review.objects.all()
     serializer_class = ReviewsSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
